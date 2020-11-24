6a720acd33e2dcce67ebcfd9e2492110431b1a8c
Adriaan Brands <a.j.m.brands@student.tudelft.nl>
2020-11-24 09:46:20 +0100

    Reworked the project structure into microservices

---
+++ AuthenticationService/src/main/java/nl/tudelft/sem10/authenticationservice/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem10.authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthenticationServiceApplication.class, args);
+  }
+
+}
---
+++ GradingService/src/test/java/nl/tudelft/sem10/gradingservice/GradingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem10.gradingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
