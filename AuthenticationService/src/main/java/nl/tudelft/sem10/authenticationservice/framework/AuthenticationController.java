6adbb56ae56dc5a93fb1b6edbe97d856e4f8043c
ethan-keller <e.keller@student.tudelft.nl>
2020-11-30 18:42:04 +0100

    refactoring + renaming endpoint

--- AuthenticationService/src/main/java/nl/tudelft/sem10/authenticationservice/framework/AuthenticationController.java
+++ AuthenticationService/src/main/java/nl/tudelft/sem10/authenticationservice/framework/AuthenticationController.java
@@ -4,12 +4,13 @@ import nl.tudelft.sem10.authenticationservice.domain.JwtRequest;
 import nl.tudelft.sem10.authenticationservice.domain.JwtResponse;
 import nl.tudelft.sem10.authenticationservice.domain.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,8 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     // use of transient for PMD
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    //    @Autowired
+    //    private AuthenticationManager authenticationManager;
     @Autowired
     private transient JwtTokenUtil jwtTokenUtil;
     @Autowired
@@ -31,10 +32,9 @@ public class AuthenticationController {
      *
      * @param request the JwtRequest that arrived at the endpoint
      * @return ResponseEntity with response
-     * @throws Exception exception if authentication fails
      */
-    @PostMapping("/authenticate")
-    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest request) throws Exception {
+    @GetMapping("/getToken")
+    public ResponseEntity<JwtResponse> getToken(@RequestBody JwtRequest request) {
         final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getNetId());
         final String token = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new JwtResponse(token));
