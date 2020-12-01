98dd16981890a382c8bc39240fb9a6fdaec04175
Stijn Coppens <s.coppens@student.tudelft.nl>
2020-12-01 23:45:23 +0100

    added first unit test

---
+++ GradingService/src/main/java/nl/tudelft/sem10/gradingservice/config/H2Config.java
@@ -0,0 +1,38 @@
+//package nl.tudelft.sem10.gradingservice.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.core.env.Environment;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.sql.DataSource;
+//import java.util.Objects;
+//
+//@Configuration
+//@EnableJpaRepositories
+//@PropertySource("classpath:application.properties")
+//@EnableTransactionManagement
+//@SuppressWarnings("unused")
+//public class H2Config {
+//
+//    @Autowired
+//    private Environment environment;
+//
+//    /**
+//     * Set up the connection to the database.
+//     */
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("jdbc.driverClassName")));
+//        dataSource.setUrl(environment.getProperty("jdbc.url"));
+//        dataSource.setUsername(environment.getProperty("jdbc.user"));
+//        dataSource.setPassword(environment.getProperty("jdbc.pass"));
+//
+//        return dataSource;
+//    }
+//}
