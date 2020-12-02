175e0a8254c53b56f35cf5925f5954338c944f86
avilakathara <A.H.Vilakathara@student.tudelft.nl>
2020-12-02 19:15:32 +0100

    Checkstyle issues fix

--- UserService/src/main/java/nl/tudelft/sem10/userservice/controllers/UserController.java
+++ UserService/src/main/java/nl/tudelft/sem10/userservice/controllers/UserController.java
@@ -1,5 +1,6 @@
 package nl.tudelft.sem10.userservice.controllers;
 
+import java.util.List;
 import nl.tudelft.sem10.userservice.entities.User;
 import nl.tudelft.sem10.userservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,77 +13,97 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 
+/**
+ * Controller for userService module. Provides basic endpoints to nteract with users database.
+ */
 @Controller
 @RequestMapping(path = "/userService")
 public class UserController {
-  @Autowired
-  private UserRepository userRepository; //NOPMD
+    @Autowired
+    private UserRepository userRepository; //NOPMD
 
-
-  @GetMapping("/users")
-  @ResponseBody
-  public List<User> users() {
-    return userRepository.getAllUsers();
-  }
-
-  @GetMapping("/userByNetId")
-  @ResponseBody
-  public User userByNetId(String netId) {
-    return userRepository.getUserByNetId(netId);
-  }
-
-  @GetMapping("/userByNetIdAndPassword")
-  @ResponseBody
-  public User userByNetIdAndPassword(String netId, String password) {
-    return userRepository.getUserByNetIdAndPassword(netId,password);
-  }
-
-  @PostMapping("/add")
-  @ResponseBody
-  public ResponseEntity<String> createUser(String netId, String password, int type) {
-    User u = userRepository.getUserByNetId(netId);
-    if (u != null) {
-      return new ResponseEntity<>(HttpStatus.IM_USED);
-    } else {
-      userRepository.insertUser(netId, password, type);
-      User n = new User(netId, password, type);
-      return new ResponseEntity<>(n.toString(), HttpStatus.CREATED);
+    /**
+     * Gets all the users in the database.
+     *
+     * @return return all users as a List of type user
+     */
+    @GetMapping("/all")
+    @ResponseBody
+    public List<User> users() {
+        return userRepository.getAllUsers();
     }
 
-    //return userRepository.insertUser(netId, password, type);
-  }
-
-  @DeleteMapping("/delete")
-  @ResponseBody
-  public ResponseEntity<String> deleteUser(String netId) {
-    User u = userRepository.getUserByNetId(netId);
-    if (u == null) {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    } else {
-      userRepository.deleteUser(netId);
-      return new ResponseEntity<>(HttpStatus.OK);
+    /**
+     * Endpoint to get one specific user.
+     *
+     * @param netId netId of the user in question
+     * @return returns the user identied with the string.
+     */
+    @GetMapping("/user")
+    @ResponseBody
+    public ResponseEntity<String> userByNetId(String netId) {
+        User u = userRepository.getUserByNetId(netId);
+        if (u == null) {
+            return new ResponseEntity<>("User does not exist", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(u.toString(), HttpStatus.OK);
+        }
     }
-  }
-  
-  @PostMapping(path = "/change")
-  @ResponseBody
-  public ResponseEntity<String> changeDetails(String netId, String password, int type) {
-    User u = userRepository.getUserByNetId(netId);
-    if (u == null) {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    } else {
-      userRepository.updateUser(netId, type, password);
-      User n = new User(netId, password, type);
-      return new ResponseEntity<>(n.toString(), HttpStatus.OK);
-    }
-  }
 
-  @GetMapping("/test")
-  @ResponseBody
-  public ResponseEntity test() {
-    return new ResponseEntity<>(HttpStatus.OK);
-  }
+    @GetMapping("/userByNetIdAndPassword")
+    @ResponseBody
+    public ResponseEntity<String> userByNetIdAndPassword(String netId, String password) {
+        User u = userRepository.getUserByNetIdAndPassword(netId, password);
+        if (u == null) {
+            return new ResponseEntity<>("User does not exist", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(u.toString(), HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public ResponseEntity<String> createUser(String netId, String password, int type) {
+        User u = userRepository.getUserByNetId(netId);
+        if (u != null) {
+            return new ResponseEntity<>(HttpStatus.IM_USED);
+        } else {
+            userRepository.insertUser(netId, password, type);
+            User n = new User(netId, password, type);
+            return new ResponseEntity<>(n.toString(), HttpStatus.CREATED);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public ResponseEntity<String> deleteUser(String netId) {
+        User u = userRepository.getUserByNetId(netId);
+        if (u == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            userRepository.deleteUser(netId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(path = "/change")
+    @ResponseBody
+    public ResponseEntity<String> changeDetails(String netId, String password, int type) {
+        User u = userRepository.getUserByNetId(netId);
+        if (u == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            userRepository.updateUser(netId, type, password);
+            User n = new User(netId, password, type);
+            return new ResponseEntity<>(n.toString(), HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public ResponseEntity test() {
+        return new ResponseEntity<>("This is a test", HttpStatus.OK);
+    }
 
 }
--- UserService/src/main/java/nl/tudelft/sem10/userservice/entities/User.java
+++ UserService/src/main/java/nl/tudelft/sem10/userservice/entities/User.java
@@ -8,113 +8,120 @@ import javax.persistence.*;
 @SuppressWarnings("unused")
 public class User {
 
-  @Id
-  @Column(name = "netid")
-  private String netId;
+    @Id
+    @Column(name = "netid")
+    private String netId;
 
-  @Column(name = "password")
-  private String password;
+    @Column(name = "password")
+    private String password;
 
-  @Column(name = "type")
-  private int type;
+    @Column(name = "type")
+    private int type;
 
-  /**
-   * Regular constructor for user.
-   *
-   * @param netId netId of the user
-   * @param password password of the user not stored as plaintext
-   * @param type type of the user, represented as an int
-   */
-  public User(String netId, String password, int type) {
-    this.netId = netId;
-    this.password = password;
-    this.type = type;
-  }
-
-  /**
-   * Default constructor for user.
-   */
-  public User() {
-    this.netId = null;
-    this.password= null;
-    this.type = -1;
-  }
-
-  /**
-   * Gets netId of user
-   * @return String netId
-   */
-  public String getNetId() {
-    return netId;
-  }
-
-  /**
-   * Gets password of user.
-   * @return String password
-   */
-  public String getPassword() {
-    return password;
-  }
-
-  /**
-   * Gets the type of user
-   * @return int type
-   */
-  public int getType() {
-    return type;
-  }
-
-  /**
-   * Set the users netId
-   * @param netId netId of user
-   */
-  public void setNetId(String netId) {
-    this.netId = netId;
-  }
-
-  /**
-   * Sets password of user
-   * @param password password of user
-   */
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  /**
-   * Set the type of user
-   * @param type type of user
-   */
-  public void setType(int type) {
-    this.type = type;
-  }
-
-  /**
-   * Standard to string method for user.
-   */
-  public String toString() {
-    //return "netID: " + netId + " Pass: " + password + " type: " + type;
-    return "{\"netId\":\"" + netId + "\", \"password\":\"" + password + "\", \"type\":\"" + type + "\"}";
-  }
-
-  /**
-   * Equals methid to check if two users are the same.
-   */
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    /**
+     * Regular constructor for user.
+     *
+     * @param netId    netId of the user
+     * @param password password of the user not stored as plaintext
+     * @param type     type of the user, represented as an int
+     */
+    public User(String netId, String password, int type) {
+        this.netId = netId;
+        this.password = password;
+        this.type = type;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    User user = (User) o;
-    return netId == user.netId
-            && type == user.type
-            && password.equals(user.password);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(netId, password, type);
-  }
+    /**
+     * Default constructor for user.
+     */
+    public User() {
+        this.netId = null;
+        this.password = null;
+        this.type = -1;
+    }
+
+    /**
+     * Gets netId of user.
+     *
+     * @return String netId
+     */
+    public String getNetId() {
+        return netId;
+    }
+
+    /**
+     * Gets password of user.
+     *
+     * @return String password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Gets the type of user.
+     *
+     * @return int type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * Set the users netId.
+     *
+     * @param netId netId of user
+     */
+    public void setNetId(String netId) {
+        this.netId = netId;
+    }
+
+    /**
+     * Sets password of user.
+     *
+     * @param password password of user
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Set the type of user.
+     *
+     * @param type type of user
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     * Standard to string method for user.
+     */
+    public String toString() {
+        //return "netID: " + netId + " Pass: " + password + " type: " + type;
+        return "{\"netId\":\"" + netId + "\", \"password\":\""
+                + password + "\", \"type\":\"" + type + "\"}";
+    }
+
+    /**
+     * Equals methid to check if two users are the same.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return netId == user.netId
+                && type == user.type
+                && password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(netId, password, type);
+    }
 }
