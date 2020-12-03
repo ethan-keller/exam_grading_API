05dd799b82410deb04f21263f3c48248c89841b1
ethan-keller <e.keller@student.tudelft.nl>
2020-12-03 22:26:41 +0100

    Resolve checktyle and PMD violations

--- AuthenticationService/src/test/java/nl/tudelft/sem10/authenticationservice/domain/JwtTokenUtilTest.java
+++ AuthenticationService/src/test/java/nl/tudelft/sem10/authenticationservice/domain/JwtTokenUtilTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+
 import java.lang.reflect.Field;
 import java.time.Clock;
 import java.util.Date;
 import nl.tudelft.sem10.authenticationservice.application.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -23,7 +23,7 @@ class JwtTokenUtilTest {
             new UserDetailsImpl(new User("jSnow", "pass123!", 0));
     private final transient UserDetails details2 =
             new UserDetailsImpl(new User("aBeller", "pass456@", 1));
-    private final transient long JWT_VALIDITY = 24 * 60 * 60;
+    private final transient long validity = 24 * 60 * 60;
     private transient JwtTokenUtil utils;
     private transient String token1;
     private transient String token2;
@@ -86,7 +86,7 @@ class JwtTokenUtilTest {
      */
     @Test
     void getExpirationDateFromToken() {
-        long d1 = (new Date(clock.millis() + 1000 * JWT_VALIDITY)).getTime() / 1000;
+        long d1 = (new Date(clock.millis() + 1000 * validity)).getTime() / 1000;
         long d2 = utils.getExpirationDateFromToken(token1).getTime() / 1000;
         assertEquals(d1, d2);
     }
@@ -102,7 +102,7 @@ class JwtTokenUtilTest {
         assertFalse(utils.validateToken(token1, details2));
 
         // mock clock to ensure expiration for testing purposes
-        long expiredTime = System.currentTimeMillis() + 1000 * JWT_VALIDITY;
+        long expiredTime = System.currentTimeMillis() + 1000 * validity;
         when(clock.millis()).thenReturn(expiredTime);
 
         // set new clock
