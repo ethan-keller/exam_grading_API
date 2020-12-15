e8e67c8d062dcbf4c3cd12e46985f463191d8ac9
Adriaan Brands <a.j.m.brands@student.tudelft.nl>
2020-12-15 23:26:57 +0100

    Update GradingServiceApplicationTests.java

--- GradingService/src/test/java/nl/tudelft/sem10/gradingservice/GradingServiceApplicationTests.java
+++ GradingService/src/test/java/nl/tudelft/sem10/gradingservice/GradingServiceApplicationTests.java
@@ -9,6 +9,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Disabled;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.tudelft.sem10.gradingservice.domain.Grade;
 import org.json.JSONArray;
